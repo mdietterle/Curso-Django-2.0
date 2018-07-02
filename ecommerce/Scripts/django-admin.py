6dc4e3bbb94a1c67514440a2351dc2955b802d0d
Martim Dietterle <dietterle@gmail.com>
2018-07-02 14:58:28 -0300

    Commit Inicial

---
+++ ecommerce/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\martim\onedrive\scripts python\curso django 2.0\ecommerce\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
